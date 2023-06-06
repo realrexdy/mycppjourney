3059435bbc7a62cbc34ccaac10d99ac38937b429
thecreativespark <134577077+thecreativespark@users.noreply.github.com>
2023-06-06 08:06:38 +0530

    Create matrix-addition.cpp

---
+++ matrix-addition.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int r, c, p, q;
+    cout << "Please enter the number of rows of the first matrix:\n";
+    cin >> r;
+    cout << "Please enter the number of columns of the first matrix:\n";
+    cin >> c;
+    float a[r][c];
+    cout << "Please enter the elements of the first matrix:\n";
+    for(int i=0; i<r; i++){
+        for(int j=0;j<c;j++){
+            cin >> a[i][j];
+        }
+    }
+    cout << "Please enter the number of rows of the second matrix:\n";
+    cin >> p;
+    if(r!=p){
+        cout << "Invalid matrix addition:\n";
+    }
+    else{
+        cout << "Please enter the number of columns of the second matrix:\n";
+    cin >> q;
+    if(c!=q){
+        cout << "Invalid matrix addition:\n";
+    }
+    else{
+        float b[p][q];
+        cout << "Please enter the elements of the second matrix:\n";
+        for(int i=0;i<p;i++){
+            for(int j=0;j<q;j++){
+                cin >> b[i][j];
+            }
+        }
+        for(int i=0;i<p;i++){
+            for(int j=0;j<q;j++){
+                cout << a[i][j]+b[i][j] << "\t";
+            }
+            cout << "\n";
+        }
+    }
+    }
+    
+}
