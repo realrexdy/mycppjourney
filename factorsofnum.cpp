e5cda9bddd97720f285d231816675cca162987a7
TheCreativeSpark <134577077+thecreativespark@users.noreply.github.com>
2023-09-24 06:10:00 +0530

    Create factorsofnum.cpp

---
+++ factorsofnum.cpp
@@ -0,0 +1,26 @@
+/*Cpp code to find the factors of a Number*/
+
+//Link Section
+#include <iostream>
+using std::cin;
+using std::cout;
+
+//Define Section
+
+// Global declaration section
+int x=0;
+int i=1;
+
+//main function
+int main(){
+    //Subprogram section
+    cout << "Please enter the number:";
+    cin >> x;
+    for(i;i<x;i++){
+        if(x%i==0){
+            cout << i <<"\n";
+        }
+        else{}
+    }
+    return 0;
+}
