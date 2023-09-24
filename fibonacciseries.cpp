b472b847cce268610af3dbc43ad11089503da065
TheCreativeSpark <134577077+thecreativespark@users.noreply.github.com>
2023-09-24 05:37:56 +0530

    Update fibonacciseries.cpp

--- fibonacciseries.cpp
+++ fibonacciseries.cpp
@@ -1,11 +1,17 @@
+/*This is a piece of cpp code to calculate the numbers in Fibonacci Series/*
+
+//Link Section
 #include <iostream>
 using std::cout;
 
+//Global Declaration
 int a=0;
 int b=1;
 int x=0;
 
+//Main Function
 int main(){
+    //Sub Program Section
     while(a<100){
        x=a+b;
        a=b;
