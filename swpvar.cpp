1b4199ae72b5b7a38d6e60ae0ef9c9b639128415
thecreativespark <guneshreddys0202@gmail.com>
2023-10-06 22:25:24 +0530

    c1

--- swpvar.cpp
+++ swpvar.cpp
@@ -5,20 +5,28 @@ using std::cin;
 using std::cout;
 
 int main(){
-
     int a,b,temp;
-    cout << "Please enter the first variable:\n";
+    int input;
+    
+    cout << "How do you want to swap your variables?\n";
+    cout << "\n" << "1.Temporary Variable\n2.Using Addition\n3.Using Division\n\n";
+    cin >> input;
+    if(input == 1){
+    cout << "\n";
+    cout << "Using Temporary Variable1\n";
+    cout << "Please enter the first variable:";
     cin >> a;
-    cout << "Please enter the second variable:\n";
+    cout << "Please enter the second variable:";
     cin >> b;
 
     temp =a;
     a=b;
     b=temp;
-    cout << "Variable 1\n";
+    cout << "Variable 1: ";
     cout << a << "\n";
-    cout << "Variable 2\n";
+    cout << "Variable 2: ";
     cout << b << "\n";
+    }
 
     return 0;
 }
