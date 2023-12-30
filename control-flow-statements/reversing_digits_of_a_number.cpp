667bc09e07cdeb453ac91c692f91c5781ca1a9cf
Your Name <your.email@example.com>
2023-12-30 12:19:51 +0530

    couple of other programs

---
+++ control-flow-statements/reversing_digits_of_a_number.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using std::cout;
+using std::cin;
+
+int main(){
+int original_num=0, reversed_num=0, temp = 0;
+cout << "Enter the number:\n";
+cin >> original_num;
+temp = original_num;
+while(original_num>0){
+    int  remainder = original_num % 10;
+    reversed_num = (reversed_num*10) + remainder;
+    original_num = original_num/10;
+}
+cout << "Reversed Number:\n";
+cout << reversed_num;
+return 0;
+}
